99d944174b438bdc086f53de1d055f6e8ec441fe
Marco <74556595+MBNRserns@users.noreply.github.com>
2025-04-15 18:50:09 +0200

    Add files via upload

---
+++ Act1.py
@@ -0,0 +1,9 @@
+#Bubble Sort
+numbs=[1,7,4,3,10,5]
+print("Before swapping :", numbs)
+for i in range(0, len(numbs)):
+    for j in range(i, len(numbs)):
+        if numbs[i]>numbs[j]:
+            numbs[i],numbs[j]=numbs[j],numbs[i]
+
+print("After swapping :", numbs)
